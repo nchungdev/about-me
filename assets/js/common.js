bcb46837cbb1acf6b3d9fd207689037f1b89dfd8
Chung Nguyen Hoai <46637152+nchungdev@users.noreply.github.com>
2019-10-14 23:37:32 +0700

    Remove blog from cv

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,59 +1,7 @@
 $(document).ready(function() {
-  (function() {
-    let hash = window.location.hash;
-    let divActive = $("#content").children("div.active");
-    const tabLg = $("#menu-lg").children("li.tab");
-    const tabSm = $("#menu-sm").children("li.tab");
-    setTabOnClickListener(tabLg);
-    setTabOnClickListener(tabSm);
-    checkIsAbout(hash);
-    function setTabOnClickListener(tabs) {
-      tabs.children("a").click(function(e) {
-        if(this.hash == undefined || this.hash == ""){
-          window.location.href = this.href;
-          return;
-        }
-        e.preventDefault();
-        checkIsAbout(this.hash);
-        window.location.hash = this.hash;
-      });
-    }
-
-    (function checkHashOnLoad() {
-      if (hash == "") {
-        $("#about").addClass("active");
-        return;
-      }
-      if ("#" + divActive.attr("id") != hash) {
-        divActive.removeClass("active");
-        $(hash).addClass("active");
-        activeTab(tabLg, hash);
-        activeTab(tabSm, hash);
-      }
-    })();
-
-    function activeTab(tabs, hash) {
-      tabs.each(function(index, value) {
-        const isEqual =
-          $(value)
-            .children("a")
-            .attr("href") == hash;
-        if (isEqual) {
-          $(value).addClass("active");
-          return;
-        }
-      });
-    }
-
-    function checkIsAbout(hash) {
-      if (hash == "#about") $("#content").addClass("no-card");
-      else $("#content").removeClass("no-card");
-      console.log("Changed.." + hash);
-    }
-  })();
-
   const headerBehavior = new HeaderBehavior($("header"), window);
   $(document).scroll(function() {
-    headerBehavior.fixedHeaderOnScroll();
+    headerBehavior.onScroll();
   });
+  headerBehavior.onLoad();
 });
--- assets/js/modules.js
+++ assets/js/modules.js
@@ -1,21 +1,82 @@
 class HeaderBehavior {
-  constructor(header, window, sticky) {
+  constructor(header, window) {
     this.header = header;
     this.window = window;
     this.sticky = this.header.offset().top;
+    this.divActive = $("#content").children("div.active");
+    this.tabLg = $("#menu-lg").children("li.tab");
+    this.tabSm = $("#menu-sm").children("li.tab");
   }
 
-  fixedHeaderOnScroll() {
+  activeTab(tabs, hash) {
+    tabs.each(function(index, value) {
+      const isEqual =
+        $(value)
+          .children("a")
+          .attr("href") == hash;
+      if (isEqual) {
+        $(value).addClass("active");
+        return;
+      }
+    });
+  }
+  setTabOnClickListener(tabs) {
+    tabs.children("a").click(function(e) {
+      const hash = this.hash;
+      if (hash == undefined || hash == "") {
+        return;
+      }
+      e.preventDefault();
+      window.location.hash = hash;
+      if (hash == "#about") $("#content").addClass("no-card");
+      else $("#content").removeClass("no-card");
+    });
+  }
+
+  onScroll() {
     if (this.window.pageYOffset > this.sticky) {
       this.header.addClass("sticky");
+      if ($(document).width() > 768) {
+        this.header.width($(".panel-background").width());
+      }
       $("#content")
         .parent()
         .addClass("sticky");
+      $(".profile").width(
+        $(".profile")
+          .parent()
+          .width()
+      );
+      $(".profile").addClass("sticky");
     } else {
       this.header.removeClass("sticky");
       $("#content")
         .parent()
         .removeClass("sticky");
+      $(".profile").removeClass("sticky");
     }
+  }
+
+  onLoad() {
+    let hash = this.window.location.hash;
+    switch (hash) {
+      case "":
+        $("#about").addClass("active");
+        return;
+      case "#about":
+        $("#content").addClass("no-card");
+        break;
+      default:
+        $("#content").removeClass("no-card");
+        break;
+    }
+    if ("#" + this.divActive.attr("id") != hash) {
+      this.divActive.removeClass("active");
+      $(hash).addClass("active");
+      this.activeTab(this.tabLg, hash);
+      this.activeTab(this.tabSm, hash);
+    }
+    this.setTabOnClickListener(this.tabLg);
+    this.setTabOnClickListener(this.tabSm);
   }
 }
