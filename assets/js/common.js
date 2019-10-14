df42cb019895ad92edda9e7ac19cf28725cf59b0
Chung Nguyen Hoai <nchungdev@users.noreply.github.com>
2019-10-14 13:49:05 +0700

    fixed load blog

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     checkIsAbout(hash);
     function setTabOnClickListener(tabs) {
       tabs.children("a").click(function(e) {
-        if(this.hash == ""){
+        if(this.hash == undefined){
           window.location.href = this.href;
           return;
         }
